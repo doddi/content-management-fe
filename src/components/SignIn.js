15a6d6e270e5b3a944cbe8be239847dfa7cf662f
Mark Dodgson <mdodgson@sonatype.com>
2019-04-02 11:13:23 +0100

    migrated to redux-form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,26 +1,63 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { Field, reduxForm } from 'redux-form';
 
 import { trySignIn } from '../actions';
 
 class SignIn extends React.Component {
-  doSignIn = () => {
-    this.props.trySignIn('admin', 'password');
+  doSignIn = ({ username, password }) => {
+    this.props.trySignIn(username, password);
   };
 
+  renderUsername(formProps) {
+    return (
+      <div className="field">
+        <label>Username:</label>
+        <input
+          {...formProps.input}
+          type="text"
+          name="username"
+          placeholder="Username"
+        />
+      </div>
+    );
+  }
+
+  renderPassword(formProps) {
+    return (
+      <div className="field">
+        <label>Password:</label>
+        <input
+          {...formProps.input}
+          type="password"
+          name="password"
+          placeholder="password"
+        />
+      </div>
+    );
+  }
+
   render() {
-    // Once logged in a re-render to redirect
+    // Once logged in a re-render will initiate this redirect
     if (this.props.signedIn) {
       return <Redirect push to="/" />;
     }
 
     return (
-      <div>
-        <input onChange={this.onEmailChange} />
-        <input onChange={this.onPasswordChange} />
-        <button onClick={this.doSignIn}>Sign In</button>
-      </div>
+      <form
+        onSubmit={this.props.handleSubmit(this.doSignIn)}
+        className="ui form"
+      >
+        <div>
+          <Field name="username" component={this.renderUsername} />
+          <Field name="password" component={this.renderPassword} />
+          <div className="field" />
+          <button className="ui button primary" type="submit">
+            Sign In
+          </button>
+        </div>
+      </form>
     );
   }
 }
@@ -31,7 +68,11 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(
-  mapStateToProps,
-  { trySignIn }
-)(SignIn);
+export default reduxForm({
+  form: 'loginForm'
+})(
+  connect(
+    mapStateToProps,
+    { trySignIn }
+  )(SignIn)
+);
