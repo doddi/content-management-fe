2c3bec89b4c6f50e322516aab6598cca352992e4
Mark Dodgson <mdodgson@sonatype.com>
2019-04-02 23:00:55 +0100

    customer list now as objects

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { fetchCustomers } from '../actions/index';
 
@@ -8,24 +9,52 @@ class CustomerList extends React.Component {
     this.props.fetchCustomers(0);
   }
 
-  render() {
-    const customerList = this.props.customers.map(customer => {
+  renderCustomers() {
+    return this.props.customers.map(customer => {
       return (
-        <div key={customer.id}>
-          {customer.firstName} {customer.lastName}
+        <div className="item" key={customer.id}>
+          <div className="right floated content">
+            <div className="ui button primary">Edit</div>
+            <div className="ui button negative ">Delete</div>
+          </div>
+          <i className="large middle aligned icon address card" />
+          <div className="content">
+            {customer.firstName} {customer.lastName}
+            <div className="description">{customer.phoneNumber}</div>
+          </div>
         </div>
       );
     });
-    return <div>{customerList}</div>;
+  }
+
+  renderCreate() {
+    //TODO Need to check if the role is admin
+    return (
+      <div style={{ textAlign: 'right' }}>
+        <Link to="/customers/new" className="ui button primary">
+          Create Customer
+        </Link>
+      </div>
+    );
+  }
+
+  render() {
+    const customerList = this.renderCustomers();
+    return (
+      <div>
+        <h2>Customers</h2>
+        <div className="ui middle aligned divided animated list">
+          {customerList}
+        </div>
+        {this.renderCreate()}
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => {
   return {
-    customers:
-      Object.entries(state.customers).length === 0
-        ? []
-        : state.customers.content
+    customers: Object.values(state.customers)
   };
 };
 
--- src/reducers/customersReducer.js
+++ src/reducers/customersReducer.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import { FETCH_CUSTOMERS } from '../actions';
 
-export default (state = [], action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_CUSTOMERS:
-      return action.payload;
+      // Produces a list of customer objects using the id as key
+      return _.mapKeys(action.payload.content, 'id');
     default:
       return state;
   }
